4bedd08826b53b5174af289667c3c5682de59590
Sebastian Korfmann <sebastian@korfmann.net>
2023-05-26 21:44:31 +0200

    Make sure to use the same version for the globally installed aws-cdk-lib

--- examples/provider-specific/awscdk-hello-wing/test.sh
+++ examples/provider-specific/awscdk-hello-wing/test.sh
@@ -11,8 +11,8 @@ function catch_error() {
 
 export CDK_STACK_NAME="hello-wing-test"
 # aws-cdk-lib is required for the AWS CDK examples, there's an issue about it https://github.com/winglang/wing/issues/2478
-npm install -g aws-cdk-lib
 npm install
+VERSION=$(npm list aws-cdk | grep aws-cdk@ | cut -d'@' -f2); npm install -g "aws-cdk-lib@$VERSION"
 wing compile -t awscdk main.w
 npx cdk deploy --require-approval never --outputs-file outputs.json --app "./target/main.awscdk"
 npx cdk destroy --force --app "./target/main.awscdk"
