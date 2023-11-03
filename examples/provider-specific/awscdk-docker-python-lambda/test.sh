4c8ce36ad7560d04ce6f949e5978bcfca8c6c441
Sebastian Korfmann <sebastian@korfmann.net>
2023-11-03 09:16:44 +0100

    Update to latest wing with platforms

--- examples/provider-specific/awscdk-docker-python-lambda/test.sh
+++ examples/provider-specific/awscdk-docker-python-lambda/test.sh
@@ -13,6 +13,6 @@ export CDK_STACK_NAME="awscdk-docker-python-lambda-test"
 # aws-cdk-lib is required for the AWS CDK examples, there's an issue about it https://github.com/winglang/wing/issues/2478
 npm install
 VERSION=$(npm list aws-cdk | grep aws-cdk@ | cut -d'@' -f2); npm install -g "aws-cdk-lib@$VERSION"
-wing compile -t awscdk main.w
+wing compile --no-analytics --no-update-check --platform awscdk main.w
 npx cdk deploy --require-approval never --outputs-file outputs.json --app "./target/main.awscdk"
 npx cdk destroy --force --app "./target/main.awscdk"
