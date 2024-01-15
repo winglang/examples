33d40d1021996170ffae92f19963ac08b0d2ed55
Sebastian Korfmann <sebastian@korfmann.net>
2024-01-15 16:43:35 +0100

    workaround for https://github.com/winglang/wing/issues/3559
    as ugly as it gets, but should return correct exit codes at least

---
+++ examples/react-website/website/run_and_kill.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+const kill = require('tree-kill');
+
+// Function to start a process and kill it after a specified duration
+function runAndKillAfter(command, args, duration) {
+    const process = spawn(command, args);
+
+    console.log(`Process started with PID: ${process.pid}`);
+
+    setTimeout(() => {
+        console.log(`Killing process with PID: ${process.pid}`);
+        const foo = kill(process.pid)
+        console.log({foo});
+        console.log(`Process killed after ${duration} milliseconds`);
+    }, duration);
+}
+
+console.log('Starting process...');
+runAndKillAfter('react-scripts', ['start'], 60000); // 60000 milliseconds = 60 seconds
