bcc81326e37e27e1f9530552b0921dd4707e980c
Sebastian Korfmann <sebastian@korfmann.net>
2024-01-26 11:23:22 +0100

    add test - it's failign though

--- examples/typescript/main.ts
+++ examples/typescript/main.ts
@@ -1,6 +1,15 @@
-import { main } from "ts4w";
+import { lift, main, std } from "ts4w";
 import { cloud } from "@winglang/sdk";
+import { equal } from "node:assert";
 
 main((app) => {
-  new cloud.Bucket(app, "Bucket");
+  let bucket = new cloud.Bucket(app, "Bucket");
+
+  bucket.addObject("hello", "Hello World from lifted Bucket!");
+
+  new std.Test(app, "test", lift({bucket}).inflight(async ({bucket}) => {
+    console.log("in test")
+    const hello = await bucket.get("hello");
+    equal(hello, "Hello World from lifted Bucket!");
+  }))
 })
